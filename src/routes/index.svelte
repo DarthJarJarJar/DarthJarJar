96b68db6300bd081a98abec66acbb08880cd3ad6
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-08-03 01:38:52 -0400

    m

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,6 @@
 <script>
 
 
-
     let opacity = -0
     let elem;
     function click() {
@@ -20,7 +19,7 @@
     <iframe id="ytplayer" type="text/html" width="640" height="360"
     src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allow="autoplay"
     frameborder="0"></iframe>
-    {:else}    <img id="rr" bind:this={elem} src={'../../src/rickroll.jpeg'} alt="rickroll">
+    {:else}    <img id="rr" bind:this={elem} src="/rickroll4k.jpeg" alt="imageTest">
 
     {/if}
 
